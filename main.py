cd03d50c5adf8e73174e39a6a448a8179667d0e5
gulnuravci <34399080+gulnuravci@users.noreply.github.com>
2022-12-10 01:43:57 -0500

    final solution :D

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ import matplotlib.pyplot as plt
         -> There is a phase difference between the sampled sequences, as modeled by the complex exponential
         -> There is added noise to both sequences
         -> Sampled signal is s(kT) with samples at integer values of k
-                                                                         
+    BONUS QUESTIONS:
+        1) Assume that the delay between the two receivers is an unknown multiple of the sample period. This is generally not the case. Why? What is going on in this assumption? How would the problem be different if this assumption was not made?                          
+            -> Making the assumption that the delay is an integer multiple of the sample period makes the calculation simpler because it can just be done by shifting the symbols, but there is no guarantee the delay will follow this rule in real life. 
+        2) There is an unknown complex phase difference between the two receivers. Why? Where does the complex phase at each of the receivers come from? 
+            ->  Because the receivers are spaced out, the signal arrives to them at different moments in time which causes the phase shift.
 """
 # constants
 SYMBOLS = 200 # number of samples to simulate
@@ -32,6 +36,10 @@ n_2 = (np.random.randn(SYMBOLS) + 1j*np.random.randn(SYMBOLS))/np.sqrt(2)
 r_1 = s + (n_1 * np.sqrt(noise_power))
 r_2 = np.roll(([val * complex_exp for val in s]), DELAY) + (n_2 * np.sqrt(noise_power))
 
+# SNR
+SNR_r_1= np.mean(np.abs(r_1)**2)/np.mean(np.abs(n_1)**2)
+SNR_r_2= np.mean(np.abs(r_2)**2)/np.mean(np.abs(n_2)**2)
+
 def plotIQ(signal, title):
     plt.figure()
     plt.title(title)
@@ -48,18 +56,23 @@ if __name__ == "__main__":
     print("     Symbols: " + str(SYMBOLS))
     print("     Unknown delay: " + str(DELAY))
     print("     Unknown phase difference (degrees): " + str(PHASE_DIFF*(180/np.pi)))
+    print()
 
     plotIQ(r_1, "Pre-processing: r_1, IQ")
     plotIQ(r_2, "Pre-processing: r_2, IQ")
-    
 
     # POST-PROCESSING
+    print("SNR signal-to-noise ratio = signal power/noise power: ")
+    print("     SNR_r_1: " + str(SNR_r_1))
+    print("     SNR_r_2: " + str(SNR_r_2))
+    print()
+
     # Find unknown delay
     corr = np.correlate(r_1 - np.mean(r_1), r_2 - np.mean(r_2), mode='full')
     lag = corr.argmax() - (len(r_1) - 1)
     print("For post-processing, we first do a cross correlation to find the time delay between the receivers. We correct for it by shifting the first receiver by the lag.")
-    print("     corr.argmax(): " + str(corr.argmax()))
     print("     lag: " + str(lag))
+    print()
 
     # Fix delay
     r_1 = np.roll(r_1, lag)
@@ -72,7 +85,9 @@ if __name__ == "__main__":
     difference = phase_2-phase_1
     print("Next, (and this is an estimation of a way I thought might mostly work) we take the mean of both receivers, which would give a single average complex number for each. I found the inner angle of the triangle the vector forms which I considered the phase of that receiver, and than I took their difference. This gives the phase difference between the receivers with an error margin of up to about 1 degree (which I took for the sake of the assignment:")
     print("     phase difference between receivers:" + str((difference*(180/np.pi))))
+    print()
     print("We adjusted for the phase shift by shifting the phase of the first receiver by the difference. The post-processing plots show how the signals are now aligned.")
+    print()
     # Fix phase shift
     complex_exp = complex(np.cos(difference), np.sin(difference))
     r_1 = [val * complex_exp for val in r_1]
